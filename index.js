af769f91177ffa9656cf1ff179db50305f937c2d
Suryansh <suusisodia56@gmail.com>
2023-04-05 09:14:28 +0530

    add a huefreelib

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ footer.innerHTML = `Copyright to @HueFree Team ${year}`;
 
 const myButton = document.getElementById('myButton');
 myButton.addEventListener('click', () => {
-    alert('I got clicked');
+    changeToAccessible(document); 
 });

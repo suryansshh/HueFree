87c2f565ae33a0ad91ddd330d4a51eebd49cfcd0
suryansh sisodia <113020704+suryansshh@users.noreply.github.com>
2023-04-12 09:21:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ const footer = document.getElementById("footer");
 const year = new Date().getFullYear();
 footer.innerHTML = `Copyright to @HueFree Team ${year}`;
 
-changeToAccessible(document); 
-   
+const myButton = document.getElementById('myButton');
+myButton.addEventListener('click', () => {
+    changeToAccessible(document); 
+});   
